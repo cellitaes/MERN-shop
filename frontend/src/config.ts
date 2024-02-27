9a501a8661082f33d96f6d8cdbf806351e65992e
Krzysztof <cellitaes@gmail.com>
2024-02-27 23:27:36 +0100

    replaced create-react-app with Vite

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-const env = process.env;
+const env = import.meta.env;
 
-export const BACKEND_URL = 'http://localhost:5000' || env.REACT_APP_BACKEND_URL;
+export const BACKEND_URL = 'http://localhost:5000' || env.BACKEND_URL;
